dc832f26026e7ce0b07980281a074da639cfeb0b
jity16 <jity16@mails.tsinghua.edu.cn>
2019-03-20 17:08:28 +0800

    bug

--- labcodes/lab1/kern/trap/trap.c
+++ labcodes/lab1/kern/trap/trap.c
@@ -146,7 +146,6 @@ print_regs(struct pushregs *regs) {
     cprintf("  eax  0x%08x\n", regs->reg_eax);
 }
 
-static int ticks = 0;
 /* trap_dispatch - dispatch based on what type of trap occurred */
 static void
 trap_dispatch(struct trapframe *tf) {
