3302a2563a6ba6f41ef28a9b8ab2b38ae9da909c
jity16 <jity16@mails.tsinghua.edu.cn>
2019-04-20 15:29:59 +0800

    lab5:add lab4 codes

--- labcodes/lab5/kern/process/proc.c
+++ labcodes/lab5/kern/process/proc.c
@@ -87,7 +87,7 @@ static struct proc_struct *
 alloc_proc(void) {
     struct proc_struct *proc = kmalloc(sizeof(struct proc_struct));
     if (proc != NULL) {
-    //LAB4:EXERCISE1 YOUR CODE
+    //LAB4:EXERCISE1 2016010308
     /*
      * below fields in proc_struct need to be initialized
      *       enum proc_state state;                      // Process state
@@ -109,6 +109,18 @@ alloc_proc(void) {
      *       uint32_t wait_state;                        // waiting state
      *       struct proc_struct *cptr, *yptr, *optr;     // relations between processes
 	 */
+      proc->state = PROC_UNINIT;
+      proc->pid = -1;
+      proc->runs = 0;
+      proc->kstack = 0;
+      proc->need_resched = 0;
+      proc->parent = NULL;
+      proc->mm = NULL;
+      memset(&(proc->context), 0, sizeof(struct context));
+      proc->tf = NULL;
+      proc->cr3 = boot_cr3;
+      proc->flags = 0;
+      memset(proc->name, 0, PROC_NAME_LEN);
     }
     return proc;
 }
@@ -370,7 +382,7 @@ do_fork(uint32_t clone_flags, uintptr_t stack, struct trapframe *tf) {
         goto fork_out;
     }
     ret = -E_NO_MEM;
-    //LAB4:EXERCISE2 YOUR CODE
+    //LAB4:EXERCISE2 2016010308
     /*
      * Some Useful MACROs, Functions and DEFINEs, you can use them in below implementation.
      * MACROs or Functions:
@@ -403,7 +415,36 @@ do_fork(uint32_t clone_flags, uintptr_t stack, struct trapframe *tf) {
 	*    update step 1: set child proc's parent to current process, make sure current process's wait_state is 0
 	*    update step 5: insert proc_struct into hash_list && proc_list, set the relation links of process
     */
-	
+	proc = alloc_proc();
+    if (proc == NULL) {
+      goto fork_out;
+    }
+    proc->parent = current;
+    if (setup_kstack(proc) != 0) {
+      goto bad_fork_cleanup_proc;
+    }
+    if (copy_mm(clone_flags, proc) != 0) {
+      goto bad_fork_cleanup_kstack;
+    }
+
+    // Copy parent's trapframe
+    // Also set syscall (exec/fork) return value to be 0.
+    copy_thread(proc, stack, tf);
+
+    bool intr_flag;
+    local_intr_save(intr_flag);
+    {
+        proc->pid = get_pid();
+        hash_proc(proc);
+        list_add(&proc_list, &(proc->list_link));
+        nr_process ++;
+    }
+    local_intr_restore(intr_flag);
+
+    wakeup_proc(proc);
+
+    ret = proc->pid;
+    
 fork_out:
     return ret;
 
