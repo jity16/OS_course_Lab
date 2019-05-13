83a3512810c5822f04fe2523b76b5a8ad18f3f4e
嵇天颖 <jity16@mails.tsinghua.edu.cn>
2019-05-13 17:16:58 +0800

    lab8:add lab7 codes

--- labcodes/lab8/kern/sync/check_sync.c
+++ labcodes/lab8/kern/sync/check_sync.c
@@ -181,9 +181,14 @@ void phi_test_condvar (i) {
 void phi_take_forks_condvar(int i) {
      down(&(mtp->mutex));
 //--------into routine in monitor--------------
-     // LAB7 EXERCISE1: YOUR CODE
+     // LAB7 EXERCISE1: 2016010308
      // I am hungry
+     state_condvar[i] = HUNGRY;
      // try to get fork
+     phi_test_condvar(i);
+     while (state_condvar[i] != EATING) {
+       cond_wait(&(mtp->cv[i]));
+     }
 //--------leave routine in monitor--------------
       if(mtp->next_count>0)
          up(&(mtp->next));
@@ -195,9 +200,12 @@ void phi_put_forks_condvar(int i) {
      down(&(mtp->mutex));
 
 //--------into routine in monitor--------------
-     // LAB7 EXERCISE1: YOUR CODE
+     // LAB7 EXERCISE1: 2016010308
      // I ate over
+     state_condvar[i]=THINKING;
      // test left and right neighbors
+     phi_test_condvar(LEFT);
+     phi_test_condvar(RIGHT);
 //--------leave routine in monitor--------------
      if(mtp->next_count>0)
         up(&(mtp->next));
